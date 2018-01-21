7f6dea866e4b86c9582734e6b404afb661f160e7
Calvin Noronha <calvin.nrnha@gmail.com>
2018-01-21 21:30:21 +0530

    General cleanup and improvements

--- app/src/main/java/com/example/calvinnoronha/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/calvinnoronha/clickcounter/MainActivity.java
@@ -2,62 +2,72 @@ package com.example.calvinnoronha.clickcounter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String ARG_COUNT = "current_count";
+
+    private TextView mCountText;
+    private long mCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View abcdView = findViewById(R.id.buttonClicker);
-        abcdView.setOnClickListener(this);
+        mCountText = findViewById(R.id.main_increment_button);
+        mCountText.setOnClickListener(this);
 
+        mCounter = savedInstanceState == null ? 0 : getSavedCount(savedInstanceState);
+        setCount();
     }
-
-    public void onClick(View arg0) {
-
-        //define a new Intent for the second Activity
-        final TextView textViewToChange = (TextView) findViewById(R.id.countText);
-        int count = Integer.parseInt(textViewToChange.getText().toString());
-        count += 1;
-        String newCount = Integer.toString(count);
-        textViewToChange.setText(newCount);
-    }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            final TextView textViewToChange = (TextView) findViewById(R.id.countText);
-            Toast.makeText(getApplicationContext(), "Counter is Reset", Toast.LENGTH_SHORT).show();
-            textViewToChange.setText("0");
-            return true;
-        }
-        else
-        {
-            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
-            startActivity(intent);
-            return true;
-        }
+        switch (item.getItemId()) {
+            case R.id.action_reset:
+                mCounter = 0;
+                setCount();
+                break;
 
+            case R.id.action_about:
+                startActivity(new Intent(this, AboutActivity.class));
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(@NonNull View v) {
+        if (v.getId() != R.id.main_increment_button) return;
+        mCounter += 1;
+        setCount();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putLong(ARG_COUNT, mCounter);
+        super.onSaveInstanceState(outState);
+    }
+
+    private long getSavedCount(@NonNull Bundle savedInstanceState) {
+        return savedInstanceState.getLong(ARG_COUNT);
+    }
+
+    private void setCount() {
+        String currentCount = mCounter == 0 ? getString(R.string.main_increment_text) : String.valueOf(mCounter);
+        mCountText.setText(currentCount);
     }
 }
